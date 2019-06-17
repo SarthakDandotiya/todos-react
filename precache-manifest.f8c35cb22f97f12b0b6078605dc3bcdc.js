30b8969cb7980aa3865ec61051014fa73bb3b9b0
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2019-06-17 15:35:05 +0530

    Updates

---
+++ precache-manifest.f8c35cb22f97f12b0b6078605dc3bcdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f1ca9b035971caeeb5460f6ec8daf7",
+    "url": "/todos-react/index.html"
+  },
+  {
+    "revision": "b88028d07a6f9d9ba75d",
+    "url": "/todos-react/static/css/main.3409822c.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/todos-react/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "b88028d07a6f9d9ba75d",
+    "url": "/todos-react/static/js/main.dfef9bf1.chunk.js"
+  },
+  {
+    "revision": "ac1d807798156c803bec",
+    "url": "/todos-react/static/js/runtime~main.2c199610.js"
+  }
+]);
